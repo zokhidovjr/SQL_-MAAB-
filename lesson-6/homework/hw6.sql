aa97f2443f58ad8bf1a19fe0b8dab4470815ee11
zokhidovjr <zabixullazohidov@gmail.com>
2025-10-01 10:36:57 +0500

    Create hw6.sql

---
+++ lesson-6/homework/hw6.sql
@@ -0,0 +1,76 @@
+CREATE TABLE InputTbl (
+    col1 VARCHAR(10),
+    col2 VARCHAR(10)
+);
+    INSERT INTO InputTbl (col1, col2) VALUES 
+('a', 'b'),
+('a', 'b'),
+('b', 'a'),
+('c', 'd'),
+('c', 'd'),
+('m', 'n'),
+('n', 'm');
+
+CREATE TABLE TestMultipleZero (
+    A INT NULL,
+    B INT NULL,
+    C INT NULL,
+    D INT NULL
+);
+
+INSERT INTO TestMultipleZero(A,B,C,D)
+VALUES 
+    (0,0,0,1),
+    (0,0,1,0),
+    (0,1,0,0),
+    (1,0,0,0),
+    (0,0,0,0),
+    (1,1,1,0);
+
+create table section1(id int, name varchar(20))
+insert into section1 values (1, 'Been'),
+       (2, 'Roma'),
+       (3, 'Steven'),
+       (4, 'Paulo'),
+       (5, 'Genryh'),
+       (6, 'Bruno'),
+       (7, 'Fred'),
+       (8, 'Andro')
+
+CREATE TABLE ProductCodes (
+    Code VARCHAR(20)
+);
+
+INSERT INTO ProductCodes (Code) VALUES
+('X-123'),
+('X_456'),
+('X#789'),
+('X-001'),
+('X%202'),
+('X_ABC'),
+('X#DEF'),
+('X-999');
+
+
+SELECT DISTINCT
+       CASE WHEN col1 < col2 THEN col1 ELSE col2 END AS col1,
+       CASE WHEN col1 < col2 THEN col2 ELSE col1 END AS col2
+FROM InputTbl;
+
+SELECT *
+FROM TestMultipleZero
+WHERE (ISNULL(A,0) + ISNULL(B,0) + ISNULL(C,0) + ISNULL(D,0)) > 0;
+
+SELECT * FROM section1
+WHERE id %2=1;
+
+SELECT TOP 1 * FROM section1
+ORDER BY id;
+
+SELECT TOP 1 * FROM section1
+ORDER BY id DESC;
+
+SELECT * FROM section1 WHERE name LIKE 'b%';
+
+SELECT * FROM ProductCodes
+WHERE Code LIKE '%[_]%';
